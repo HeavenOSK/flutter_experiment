c1ea02d99b0d418542cf37d25c03d73e94fc6245
heavenOSK <ryunosukeheaven.ba@gmail.com>
2020-08-09 10:40:20 +0900

    fix: async_result_main

--- lib/main_async_result.dart
+++ lib/main_async_result.dart
@@ -3,13 +3,25 @@ import 'package:http/http.dart' as http;
 
 const _url = "https://api.github.com/repositories/31792824/issues";
 
-class GithubApiClient {}
+class CustomException implements Exception {}
 
 void main() async {
   final result = await Result.capture(http.get(_url));
-  if (result.isValue) {
-    print(result.asValue.value);
+
+  if (result.isError) {
+    switch (result.asError.error.runtimeType) {
+      case CustomException:
+        print('Custom Exception');
+        break;
+      case http.ClientException:
+        print('Client Exception');
+        break;
+      default:
+        print('Unexpected Error');
+        break;
+    }
   } else {
-    print(result.asError.error);
+    final response = result.asValue.value;
+    print(response.body);
   }
 }
