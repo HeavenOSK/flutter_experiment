d853a001de00f39a1282e9da32985dce9e70ab6c
heavenOSK <ryunosukeheaven.ba@gmail.com>
2020-10-14 01:53:41 +0900

    refactor stateType

--- lib/ipad14_like_mouse_cursor/virtual_mouse_cursor_presenter/positioned_mouse_cursor.dart
+++ lib/ipad14_like_mouse_cursor/virtual_mouse_cursor_presenter/positioned_mouse_cursor.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_experiment/ipad14_like_mouse_cursor/virtual_mouse_cursor_presenter/mouse_cursor_information.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -33,47 +34,42 @@ class PositionedMouseCursor extends StatefulHookWidget {
 class _PositionedMouseCursorState extends State<PositionedMouseCursor>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  Animation<Offset> _position;
+  Animation<Size> _size;
+
+  _CursorStateType _stateType = _CursorStateType.noFocus;
+  set stateType(value) {
+    if (_stateType != value) {
+      _stateType = value;
+      print('_stateType:$_stateType');
+    }
+  }
+
   @override
   void initState() {
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 600),
+      duration: Duration(milliseconds: 100),
     );
     super.initState();
   }
 
-  _CursorStateType _stateType = _CursorStateType.noFocus;
-  set stateType(value) {
-    if (_stateType == value) {
-      return;
-    }
-    _stateType = value;
-    switch (_stateType) {
-      case _CursorStateType.startFocus:
-        _forward();
-        break;
-      case _CursorStateType.focus:
-        break;
-      case _CursorStateType.removeFocus:
-        _reverse();
-        break;
-      case _CursorStateType.noFocus:
-        break;
-    }
-  }
-
-  void startFocus() {
+  void _startFocus(MouseCursorInformation info) {
     if (_stateType.hasFocus) {
       return;
     }
     stateType = _CursorStateType.startFocus;
+    _initializeAnimation(info);
+    _forward();
   }
 
-  void removeFocus() {
+  void _removeFocus(MouseCursorInformation info) {
     if (!_stateType.hasFocus) {
       return;
     }
     stateType = _CursorStateType.removeFocus;
+    _initializeAnimation(info);
+    _reverse();
   }
 
   void _forward() {
@@ -83,9 +79,27 @@ class _PositionedMouseCursorState extends State<PositionedMouseCursor>
   }
 
   void _reverse() {
-    _controller.forward().then((_) {
+    _controller.reverse().then((_) {
       stateType = _CursorStateType.noFocus;
     });
+  }
+
+  void _initializeAnimation(MouseCursorInformation info) {
+    _position = _controller.drive(
+      Tween(
+        begin: info.realPosition,
+        end: info.target.position,
+      ),
+    );
+    _size = _controller.drive(
+      Tween(
+        begin: Size(
+          PositionedMouseCursor.radius,
+          PositionedMouseCursor.radius,
+        ),
+        end: info.target.targetSize,
+      ),
+    );
   }
 
   @override
@@ -100,22 +114,35 @@ class _PositionedMouseCursorState extends State<PositionedMouseCursor>
         animation: _controller,
         builder: (context, snapshot) {
           if (info.hasFocus) {
-            startFocus();
+            _startFocus(info);
           } else {
-            removeFocus();
+            _removeFocus(info);
           }
 
           final isAnimating = _stateType.animating;
+          final hasFocus = _stateType.hasFocus;
 
-          final pos = info.realPosition;
-          final size = Size(
-            PositionedMouseCursor.radius,
-            PositionedMouseCursor.radius,
-          );
+          final pos = isAnimating
+              ? _position.value
+              : (hasFocus
+                  ? info.target.position
+                  : info.realPosition -
+                      Offset(
+                        PositionedMouseCursor.radius,
+                        PositionedMouseCursor.radius,
+                      ));
+          final size = isAnimating
+              ? _size.value
+              : (hasFocus
+                  ? info.target.targetSize
+                  : Size(
+                      PositionedMouseCursor.radius,
+                      PositionedMouseCursor.radius,
+                    ));
 
           return Positioned(
-            top: pos.dy - PositionedMouseCursor.radius,
-            left: pos.dx - PositionedMouseCursor.radius,
+            top: pos.dy,
+            left: pos.dx,
             child: IgnorePointer(
               child: Container(
                 height: size.height,
